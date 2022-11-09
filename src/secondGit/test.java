c49b5e5c49121a8c75abbff6da9863d77a9b54d1
Sadia <sadia.alimi1@gmail.com>
2022-11-08 21:53:40 -0500

    this is practice

---
+++ src/secondGit/test.java
@@ -0,0 +1,15 @@
+package secondGit;
+
+public class test {
+public static void main(String[] args) {
+		
+	System.out.print("salaam ");
+		
+		
+		
+	}
+
+	public void test1() {
+		System.out.print("hellow ");
+	}
+}
